e5476416ed3cbd669a540977b1f143cdbe345c26
siyumy80 <siyumy80@gmail.com>
2022-07-23 18:17:37 +0900

    Revert "-- 실습"
    This reverts commit ee8e70a5e1d047948bafeba05430da727cb89e78.

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,3 @@
 /**
  * TODO: IE11에서도 동작하는 자바스크립트로 변환할수 있게 바벨을 구성하세요.
  */
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          chrome: "80",
-          ie: "11",
-        },
-        useBuiltIns: "usage",
-        corejs: {
-          version: 3,
-        },
-      },
-    ],
-  ],
-};
