077c71b9a8292bfe2673abc1583fe8aee8bc0571
siyumy80 <siyumy80@gmail.com>
2022-07-23 18:26:50 +0900

    Revert "Revert "Revert "-- 실습"""
    This reverts commit d6ef093918e27e8a9edd7fbef4471fca437bbab8.

--- webpack.config.js
+++ webpack.config.js
@@ -10,69 +10,57 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js",
+    main: "./src/app.js"
   },
   output: {
     filename: "[name].js",
-    path: path.resolve("./dist"),
+    path: path.resolve("./dist")
   },
   module: {
     rules: [
       {
-        test: /\.css$/, //처리해야 할 파일 패턴
+        test: /\.(scss|css)$/,
         use: [
           process.env.NODE_ENV === "production"
-            ? MiniCssExtractPlugin.loader
-            : "style-loader",
-          "css-loader",
-        ], //순서는 뒤에서 부터 앞 순서로 적용
-      },
-      //import 해서 사용할 경우.
-      {
-        test: /\.(png|jpg|gif|svg)$/i, //처리해야 할 파일 패턴
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024,
-          },
-        },
+            ? MiniCssExtractPlugin.loader // 프로덕션 환경
+            : "style-loader", // 개발 환경
+          "css-loader"
+        ]
       },
       {
-        test: /\.js$/,
-        use: [
-          {
-            loader: "babel-loader",
-          },
-        ],
-        //절대경로로 지정.
-        exclude: [path.resolve(__dirname, "./node_modules")],
-      },
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 10000 // 10Kb
+        }
+      }
       /**
        * TODO: babel-loader를 구성해 보세요.
        */
-    ],
+    ]
   },
   plugins: [
     new webpack.BannerPlugin({
-      banner: `빌드 날짜: ${new Date().toLocaleString()}`,
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`
     }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
       },
       minify:
         process.env.NODE_ENV === "production"
           ? {
               collapseWhitespace: true, // 빈칸 제거
-              removeComments: true, // 주석 제거
+              removeComments: true // 주석 제거
             }
           : false,
-      hash: process.env.NODE_ENV === "production",
+      hash: process.env.NODE_ENV === "production"
     }),
     new CleanWebpackPlugin(),
     ...(process.env.NODE_ENV === "production"
       ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
-      : []),
-  ],
+      : [])
+  ]
 };
